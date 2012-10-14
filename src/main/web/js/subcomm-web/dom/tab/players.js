e1ef5596cec31b7f18f0c704758a93ccc526b445
roylaurie <roy.laurie@gmail.com>
2012-10-14 15:17:31 -0700

    entering/leaving messages appear on banner. player sorting is case-insensitive. player tab complete

--- src/main/web/js/subcomm-web/dom/tab/players.js
+++ src/main/web/js/subcomm-web/dom/tab/players.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
 	$('.subcommContainer').bind('subcommMessage', function(event, data) {		
 		function entering(player) {
 			var html = '<p>' + player + '</p>';
+			player = player.toLowerCase();
 			$('.subcommHistoryPlayers').each(function(panelIndex, panel) {
 				var done = false;
 				$(this).children('p').each(function(index, element) { // attempt to insert before this <p>
-					if (!done && player < $(this).html()) {
+					if (!done && player < $(this).html().toLowerCase()) {
 						$(this).before(html);
 						$(panel).scrollTop($(panel)[0].scrollHeight);
 						done = true;
@@ -40,4 +41,25 @@ $(document).ready(function() {
 		}
 	});
 	
+	$('.subcommContainer').bind('subcommBannerMessage', function(event, data) {
+		var container = data.container;
+		var message = data.message;
+		var action = 'entered';
+		var matches = message.match(/^(?:ENTERING):(.+?):.+?:.+?$/);
+		if (!matches) {
+			action = 'left';
+			matches = message.match(/^LEAVING:(.+?)$/);
+			if (!matches) {
+				return;
+			}
+		}
+		
+		var player = matches[1];
+		var html = '<p><span style="color: blue;">' + player + '</span> has ' + action + ' the arena'
+		$('.subcommBanner').each(function(index, element) {
+			$(this).html(html);
+		});
+		
+		event.stopImmediatePropagation();
+	});
 });
