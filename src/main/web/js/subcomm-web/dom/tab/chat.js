6c50f249d2a1e14011c1394de3cfa4a472014811
roylaurie <roy.laurie@gmail.com>
2012-10-13 18:08:13 -0700

    urls are linkified in chat tab.

--- src/main/web/js/subcomm-web/dom/tab/chat.js
+++ src/main/web/js/subcomm-web/dom/tab/chat.js
@@ -5,34 +5,58 @@ $(document).ready(function() {
 	 * @param message
 	 * @returns string
 	 */
-    function createMessageHtml(container, message, showTimestamp) {
-    	var matches = message.match(/^MSG:(.+?):(.+?):(.+)$/);
-		if (!matches) {
+    function createMessageHtml(container, message, showTimestamp, limitContent) {
+    	if (!message.match(/^MSG:/)) {
 			return;
 		}
-		
-		showTimestamp = ( typeof(showTimestamp) !== 'undefined' ? showTimestamp : true );
+    	
+    	showTimestamp = ( typeof(showTimestamp) !== 'undefined' ? showTimestamp : true );
+    	limitContent = ( typeof(limitContent) !== 'undefined' ? limitContent : false );
 		var timestamp = ( showTimestamp ? SubcommUIUtility.makeTimestamp() : '' );
-		var target = matches[1];
-		var origin = matches[2];
 		
-		var content = matches[3];
-		if (content.length > 80)
-			content = content.substring(0, 80);
-		
-		var color;
-		switch (target) {
-		case 'PUB':
-			color = 'blue';
-			break;
-		case 'FREQ':
-			color = 'yellow';
-			break;
-		default:
-			color = 'orange';
+    	var matches = message.match(/^MSG:ARENA:(.+)$/);
+		if (matches) {
+			return arenaHtml();
 		}
 		
-		return '<p>' + timestamp + '<span style="color: ' + color + ';">&lt;' + origin + '&gt;</span> ' + content + '</p>';
+    	matches = message.match(/^MSG:(.+?):(.+?):(.+)$/);
+    	if (matches) {
+    		return pubHtml();
+    	}
+    	
+    	function arenaHtml() {
+    		var content = formatContent(matches[1]);
+    		return '<p>' + timestamp + '<span style="color: green;">' + content + '</span></p>';
+    	}
+    	
+    	function pubHtml() {
+    		var target = matches[1];
+    		var origin = matches[2];
+    		var content = formatContent(matches[3]);
+    		
+    		var color;
+    		switch (target) {
+    		case 'PUB':
+    			color = 'blue';
+    			break;
+    		case 'FREQ':
+    			color = 'yellow';
+    			break;
+    		default:
+    			color = 'orange';
+    		}
+    		
+    		return '<p>' + timestamp + '<span style="color: ' + color + ';">&lt;' + origin + '&gt;</span> ' + content + '</p>';
+    	}
+    	
+    	function formatContent(content) {
+    		if (limitContent && content.length > 80)
+    			content = content.substring(0, 80);
+
+    		// taken from https://github.com/uudashr/jquery-linkify/blob/master/jquery.linkify.js
+    		content = content.replace(/((http|https|ftp)\:\/\/|\bw{3}\.)[a-z0-9\-\.]+\.[a-z]{2,3}(:[a-z0-9]*)?\/?([a-z0-9\-\._\?\,\'\/\\\+&amp;%\$#\=~])*/gi, "<a href=\"$&\" target=\"_blank\">$&</a>");
+    		return content;
+    	}
     }
     
 	$('.subcommContainer').bind('subcommMessage', function(event, data) {
@@ -55,7 +79,7 @@ $(document).ready(function() {
 	$('.subcommContainer').bind('subcommBannerMessage', function(event, data) {
 		var container = data.container;
 		var message = data.message;
-		var messageHtml = createMessageHtml(container, message, false);
+		var messageHtml = createMessageHtml(container, message, false, true);
 		if (!messageHtml) {
 			return;
 		}
